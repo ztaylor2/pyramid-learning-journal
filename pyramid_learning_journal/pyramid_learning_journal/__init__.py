badc9e569e10c64148417df40ca6555ad99177f1
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-30 16:39:49 -0700

    sample.html file is rendering to browser.

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -8,5 +8,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
+    config.include('.views')
     config.scan()
     return config.make_wsgi_app()
