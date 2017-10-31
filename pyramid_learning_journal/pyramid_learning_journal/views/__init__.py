992ec170a1b3ec25f390845195a0dd8a534a3c84
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-30 18:37:02 -0700

    tried to fix routes for heroku.

--- pyramid_learning_journal/pyramid_learning_journal/views/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/__init__.py
@@ -6,7 +6,7 @@ from .default import update_view
 
 def includeme(config):
     """List of views to include for the configurator object."""
-    config.add_view(list_view, route_name='home')
+    config.add_view(list_view, route_name='')
     config.add_view(detail_view, route_name='detail')
     config.add_view(create_view, route_name='create')
     config.add_view(update_view, route_name='update')
