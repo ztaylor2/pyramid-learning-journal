26080e6ee0d2ab77d6d3caf68f4e43601283ad67
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-01 19:07:37 -0700

    passing all tests.

--- pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/tests.py
@@ -1,87 +1,63 @@
+"""Tests for pyramid app."""
 from pyramid import testing
-from pyramid.response import Response
+from pyramid_learning_journal.data.data import ENTRIES
+import pytest
 
 
-def test_home_view_returns_response():
+def test_list_view_returns_dict():
     """Home view returns a Response object."""
     from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
-    assert isinstance(response, Response)
+    assert isinstance(response, dict)
 
-def test_home_view_is_good():
-    """Home view response has a status 200 OK."""
-    from pyramid_learning_journal.views.default import list_view
-    request = testing.DummyRequest()
-    response = list_view(request)
-    assert response.status_code == 200
 
-def test_home_view_returns_proper_content():
+def test_list_view_returns_proper_amount_of_content():
     """Home view response has file content."""
     from pyramid_learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
-    assert "<main class=\"article-list\">" in response.text
+    assert len(response['entries']) == len(ENTRIES)
 
-def test_detail_view_returns_response():
-    """Detail view returns a Response object."""
+
+def test_detail_view():
+    """Test that what's returned by the view is a dictionary of values."""
     from pyramid_learning_journal.views.default import detail_view
     request = testing.DummyRequest()
-    response = detail_view(request)
-    assert isinstance(response, Response)
+    request.matchdict['id'] = 5
+    info = detail_view(request)
+    assert isinstance(info, dict)
 
-def test_detail_view_is_good():
-    """Detail view response has a status 200 OK."""
+
+def test_detail_view_response_contains_expense_attrs():
+    """Test that what's returned by the view contains one expense object."""
     from pyramid_learning_journal.views.default import detail_view
     request = testing.DummyRequest()
-    response = detail_view(request)
-    assert response.status_code == 200
+    request.matchdict['id'] = 5
+    info = detail_view(request)
+    for key in ["id", "title", "body", "creation_date"]:
+        assert key in info["entry"]
 
-def test_detail_view_returns_proper_content():
-    """Detail view response has file content."""
-    from pyramid_learning_journal.views.default import detail_view
-    request = testing.DummyRequest()
-    response = detail_view(request)
-    assert "<main class=\"article-content\">" in response.text
 
-def test_create_view_returns_response():
-    """Detail view returns a Response object."""
-    from pyramid_learning_journal.views.default import create_view
-    request = testing.DummyRequest()
-    response = create_view(request)
-    assert isinstance(response, Response)
+@pytest.fixture()
+def testapp():
+    """Create an instance of our app for testing."""
+    from pyramid_learning_journal import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
 
-def test_create_view_is_good():
-    """Detail view response has a status 200 OK."""
-    from pyramid_learning_journal.views.default import create_view
-    request = testing.DummyRequest()
-    response = create_view(request)
-    assert response.status_code == 200
 
-def test_create_view_returns_proper_content():
-    """Detail view response has file content."""
-    from pyramid_learning_journal.views.default import create_view
-    request = testing.DummyRequest()
-    response = create_view(request)
-    assert "<label for=\"exampleFormControlTextarea1\">Article Body</label>" in response.text
+def test_layout_root(testapp):
+    """Test that the contents of the root page contains <article>."""
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert 'Today we learned about a binary heap.' in html.find("p").text
 
-def test_update_view_returns_response():
-    """Detail view returns a Response object."""
-    from pyramid_learning_journal.views.default import update_view
-    request = testing.DummyRequest()
-    response = update_view(request)
-    assert isinstance(response, Response)
 
-def test_update_view_is_good():
-    """Detail view response has a status 200 OK."""
-    from pyramid_learning_journal.views.default import update_view
-    request = testing.DummyRequest()
-    response = update_view(request)
-    assert response.status_code == 200
-
-def test_update_view_returns_proper_content():
-    """Detail view response has file content."""
-    from pyramid_learning_journal.views.default import update_view
-    request = testing.DummyRequest()
-    response = update_view(request)
-    assert "<label for=\"exampleFormControlTextarea1\">Article Body</label>" in response.text
+def test_root_contents(testapp):
+    """Test that the contents of the root page contains as many <p> tags as expenses."""
+    from pyramid_learning_journal.data.data import ENTRIES
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(ENTRIES) == len(html.findAll("p"))
