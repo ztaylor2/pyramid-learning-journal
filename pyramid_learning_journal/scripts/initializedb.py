5f608fdd8ac95de1f2efe592fc4f709bcb3f877c
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-03 11:43:52 -0700

    forgot to include the database url in the initialize db file

--- pyramid_learning_journal/scripts/initializedb.py
+++ pyramid_learning_journal/scripts/initializedb.py
@@ -33,7 +33,8 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
-
+    if os.environ.get('DATABASE_URL', ''):
+        settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     engine = get_engine(settings)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
