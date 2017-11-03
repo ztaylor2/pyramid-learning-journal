58f2df135622ad5e73e8f6064d63725b87fb7356
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-03 09:10:16 -0700

    moved url decleration to runapp.py.

--- pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/__init__.py
@@ -2,15 +2,18 @@ import os
 
 from pyramid.config import Configurator
 
+# from urllib import parse
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
 
+    # url = parse.urlparse(os.environ["DATABASE_URL"])
 
-
-    if os.environ.get('DATABASE_URL', ''):
-        settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    # settings = {}
+    # if os.environ.get('DATABASE_URL', ''):
+    #     settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
--- runapp.py
+++ runapp.py
@@ -4,19 +4,25 @@ from paste.deploy import loadapp
 from waitress import serve
 
 
-from urllib import parse
-import psycopg2
+# from urllib import parse
+# import psycopg2
 
-parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])
+# parse.uses_netloc.append("postgres")
+# url = parse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+# conn = psycopg2.connect(
+#     database=url.path[1:],
+#     user=url.username,
+#     password=url.password,
+#     host=url.hostname,
+#     port=url.port
+# )
+
+
+settings = {}
+if os.environ.get('DATABASE_URL', ''):
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+settings['pyramid.includes'] = ['pyramid_tm']
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
