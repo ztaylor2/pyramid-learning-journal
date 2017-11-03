b82106d1f66cecbed074ee2b70593d85d3a749a4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-02 23:06:59 -0700

    added code to runapp.py for heroku.

--- pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/__init__.py
@@ -14,3 +14,5 @@ def main(global_config, **settings):
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
+
+
--- runapp.py
+++ runapp.py
@@ -3,6 +3,23 @@ import os
 from paste.deploy import loadapp
 from waitress import serve
 
+
+from urllib import parse
+import psycopg2
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
