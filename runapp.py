49bc2ad97e22628af799a0ba1620915716183d9a
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-02 23:32:40 -0700

    added code to runapp.py

--- runapp.py
+++ runapp.py
@@ -2,6 +2,22 @@ import os
 
 from paste.deploy import loadapp
 from waitress import serve
+from urllib import parse
+import psycopg2
+
+
+# import os
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
